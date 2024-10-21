b121f80e83dec1f4f93ecd317a8e02f277296a54
shivamy009 <shivam0342005@gmail.com>
2024-10-21 15:19:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+const cors=require('cors')
+app.use(cors())
+
+require('dotenv').config()
+app.use(express.json())
+const database=require('../server/config/database')
+const userRoute=require('../server/router/userRoute')
+
+database()
+
+app.use('/api/v1/auth',userRoute)
+app.use('/ty',(req,res)=>{
+    res.send("Hello")
+})
+ 
+const PORT = process.env.PORT || 8001
+app.use('/ty',(req,res)=>{
+    res.send("Hell")
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is listening on port ${PORT}`)
+})
